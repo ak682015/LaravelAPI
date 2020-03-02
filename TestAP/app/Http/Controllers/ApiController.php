661a81782802bcc983ed3f3da6acd40a1322fafa
arman68 <arman.khan@projectasfalis.in>
2020-03-03 01:02:40 +0530

    Create Operation

---
+++ TestAP/app/Http/Controllers/ApiController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Student;
+
+class ApiController extends Controller
+{
+    public function create(Request $request)
+    {
+        $students = new Student();
+
+        $students->fname = $request->input('fname');
+        $students->lname = $request->input('lname');
+        $students->email = $request->input('email');
+        $students->password = $request->input('password');
+
+        $students->save();
+
+        return response()->json($students);
+
+    }
+
+    // public function show(Request $request)
+    // {
+    //     // $students = Student::where('fname', $request->input('fname'))->get();
+    //     // $students = Student::where('fname', $request->('fname'))->get();
+    //     $students = Student::where('fname', $request->input('fname'))->get();
+
+
+    //     return response()->json($students);
+    // }
+
+
+    public function show(Request $request)
+    {
+        // $students = Student::where('fname', $request->input('fname'))->get();
+        // $students = Student::where('fname', $request->('fname'))->get();
+        $students = Student::where('fname', $request->input('fname'))->get();
+
+
+        return response()->json($students);
+    }
+}
